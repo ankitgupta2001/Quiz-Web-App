717a056e5ebabdd2fff007d99836c649b0540f02
Ankit Gupta <ankitgupta97481@gmail.com>
2024-08-19 22:20:21 +0530

    Create UserQuiz.java

---
+++ Quiz-Web-App/backend/src/main/java/com/quiz/backend/entity/UserQuiz.java
@@ -0,0 +1,31 @@
+package com.quiz.backend.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_quiz")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserQuiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "completed_at")
+    private LocalDateTime completedAt;
+
+    @Column(name = "score", nullable = false)
+    private int score;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "quiz_id", nullable = false)
+    private Quiz quiz;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
