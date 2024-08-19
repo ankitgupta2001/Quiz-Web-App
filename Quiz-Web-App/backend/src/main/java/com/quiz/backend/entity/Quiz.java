69cef707b882eb1976799a809f8b797ab6c08752
Ankit Gupta <ankitgupta97481@gmail.com>
2024-08-19 22:19:16 +0530

    Create Quiz.java

---
+++ Quiz-Web-App/backend/src/main/java/com/quiz/backend/entity/Quiz.java
@@ -0,0 +1,42 @@
+package com.quiz.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "quiz")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "version", nullable = false)
+    private int version;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "creator_id", nullable = false)
+    @JsonIgnore
+    private User user;
+}
