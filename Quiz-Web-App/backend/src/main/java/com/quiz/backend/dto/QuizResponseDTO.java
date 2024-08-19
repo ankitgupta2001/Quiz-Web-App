333de9a09f1a4bc4f236d649ec64cc3675f3f912
Ankit Gupta <ankitgupta97481@gmail.com>
2024-08-19 22:44:56 +0530

    Create QuizResponseDTO.java

---
+++ Quiz-Web-App/backend/src/main/java/com/quiz/backend/dto/QuizResponseDTO.java
@@ -0,0 +1,17 @@
+package com.quiz.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class QuizResponseDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String code;
+
+    private Long userId;
+}
