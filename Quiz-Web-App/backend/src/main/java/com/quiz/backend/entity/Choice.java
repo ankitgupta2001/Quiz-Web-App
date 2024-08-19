75d7610b153c4c175b8e8ef3ef56aa44c9f83e9c
Ankit Gupta <ankitgupta97481@gmail.com>
2024-08-19 22:17:20 +0530

    Create Choice.java

---
+++ Quiz-Web-App/backend/src/main/java/com/quiz/backend/entity/Choice.java
@@ -0,0 +1,26 @@
+package com.quiz.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "choices")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Choice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "is_correct", nullable = false)
+    private boolean correct;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+}
