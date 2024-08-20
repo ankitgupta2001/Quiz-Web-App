cc49f78132d5ee6c152e31a8bfb81617d44a010f
Ankit Gupta <ankitgupta97481@gmail.com>
2024-08-20 23:38:23 +0530

    Create QuestionRepository.java

---
+++ Quiz-Web-App/backend/src/main/java/com/quiz/backend/repository/QuestionRepository.java
@@ -0,0 +1,16 @@
+package com.quiz.backend.repository;
+
+import com.quiz.backend.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findByQuiz_Id(Long quizId);
+
+    List<Question> findByQuiz_IdAndQuiz_User_Id(Long quizId, Long userId);
+
+    Question findByQuiz_IdAndQuiz_User_IdAndId(Long quizId, Long userId, Long questionId);
+
+    void deleteByQuiz_Id(Long quizId);
+}
