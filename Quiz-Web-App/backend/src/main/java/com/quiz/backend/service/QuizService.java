005480e8e90737fb52ee48ffd32efbbee9f72840
Ankit Gupta <ankitgupta97481@gmail.com>
2024-08-20 23:44:22 +0530

    Create QuizService.java

---
+++ Quiz-Web-App/backend/src/main/java/com/quiz/backend/service/QuizService.java
@@ -0,0 +1,19 @@
+package com.quiz.backend.service;
+
+import com.quiz.backend.dto.QuizDTO;
+import com.quiz.backend.dto.QuizResponseDTO;
+import com.quiz.backend.entity.Quiz;
+
+import java.util.List;
+
+public interface QuizService {
+    List<QuizResponseDTO> getAllQuiz(Long userId);
+
+    Quiz saveQuiz(QuizDTO newQuiz, Long userId);
+
+    void deleteQuiz(Long userId, Long quizID);
+
+    Quiz updateQuiz(Long quizId, Long userId, QuizDTO updatedQuiz);
+
+    QuizResponseDTO getQuiz(String code);
+}
